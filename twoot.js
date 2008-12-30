8eb89ff91825443573ae8fbc50b3e8dae83c0617
Dr. Drang <drdrang@gmail.com>
2008-12-30 12:11:02 -0600

    Code cleanup. Comments at closing ) & } in gettweets.

--- twoot.js
+++ twoot.js
@@ -14,30 +14,30 @@ jQuery.fn.reverse = function() {
 
 (function($) {
  $.fn.gettweets = function(){
- 	return this.each(function(){
-		 var list = $('ul.tweet_list').prependTo(this);
-		 var url = 'http://twitter.com/statuses/friends_timeline.json?page=' + PAGE + getSinceParameter();
-		 
-		 $.getJSON(url, function(data){
-			 $.each(data.reverse(), function(i, item) { 
-				if($("#msg-" + item.id).length == 0) { // <- fix for twitter caching which sometimes have problems with the "since" parameter
+  return this.each(function(){
+     var list = $('ul.tweet_list').prependTo(this);
+     var url = 'http://twitter.com/statuses/friends_timeline.json?page=' + PAGE + getSinceParameter();
+     
+     $.getJSON(url, function(data){
+       $.each(data.reverse(), function(i, item) { 
+        if($("#msg-" + item.id).length == 0) { // <- fix for twitter caching which sometimes have problems with the "since" parameter
           list.prepend('<li id="msg-' + item.id + '">' +
-				 	'<img class="profile_image" src="' + 
-				 	  item.user.profile_image_url + '" alt="' + item.user.name + '" />' +
-				 	'<span class="time" title="' + item.created_at + '">' +
-				 	  relative_time(item.created_at) + '</span> '+
-				 	'<a class="user" href="http://twitter.com/' + 
-				 	  item.user.screen_name + '">' +
-				 	item.user.screen_name + '</a> ' +
-				 	'<a class="favorite" title="Toggle favorite status" '+
-				 	  'href="javascript:toggleFavorite(' + 
-				 	  item.id + ')">&#10029;</a>' +
+          '<img class="profile_image" src="' + 
+            item.user.profile_image_url + '" alt="' + item.user.name + '" />' +
+          '<span class="time" title="' + item.created_at + '">' +
+            relative_time(item.created_at) + '</span> '+
+          '<a class="user" href="http://twitter.com/' + 
+            item.user.screen_name + '">' +
+          item.user.screen_name + '</a> ' +
+          '<a class="favorite" title="Toggle favorite status" '+
+            'href="javascript:toggleFavorite(' + 
+            item.id + ')">&#10029;</a>' +
           '<a class="reply" title="Reply to this" ' +
             'href="javascript:replyTo(\'' +
             item.user.screen_name + '\',' + item.id +
             ')">@</a>' +
           '<div class="tweet_text">' +
-				 	item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/g, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/g, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/g, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
+          item.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/g, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/g, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/g, "<tt class='heart'>&#x2665;</tt>") + '</div></li>');
 
           // Change the class if it's a favorite.
           if (item.favorited) {
@@ -55,87 +55,87 @@ jQuery.fn.reverse = function() {
           // The Older link is always visible after the tweets are shown.
           $("#older").css("visibility", "visible");
             
-					// Don't want Growl notifications? Comment out the following method call
-					fluid.showGrowlNotification({
-						title: item.user.name + " @" + item.user.screen_name,
-						description: item.text,
-						priority: 2,
-						icon: item.user.profile_image_url
-					});
+          // Don't want Growl notifications? Comment out the following method call
+          fluid.showGrowlNotification({
+            title: item.user.name + " @" + item.user.screen_name,
+            description: item.text,
+            priority: 2,
+            icon: item.user.profile_image_url
+          });
 
-					}
-				 });
-			 });
-		 });
- };
+          }  // if
+         }); // each
+       }); // getJSON
+     }); // this.each
+ };  // gettweets
 })(jQuery);
 
 
 function relative_time(time_value) {
-	var values = time_value.split(" ");
-	time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-	var parsed_date = Date.parse(time_value);
-	var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-	var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-	delta = delta + (relative_to.getTimezoneOffset() * 60);
-	if (delta < 60) {
-		return 'less than a minute ago';
-	} else if(delta < 120) {
-		return 'a minute ago';
-	} else if(delta < (45*60)) {
-		return (parseInt(delta / 60)).toString() + ' minutes ago';
-	} else if(delta < (90*60)) {
-		return 'an hour ago';
-	} else if(delta < (24*60*60)) {
-		return '' + (parseInt(delta / 3600)).toString() + ' hours ago';
-	} else if(delta < (48*60*60)) {
-		return '1 day ago';
-	} else {
-		return (parseInt(delta / 86400)).toString() + ' days ago';
-	}
+  var values = time_value.split(" ");
+  time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+  var parsed_date = Date.parse(time_value);
+  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+  delta = delta + (relative_to.getTimezoneOffset() * 60);
+  if (delta < 60) {
+    return 'less than a minute ago';
+  } else if(delta < 120) {
+    return 'a minute ago';
+  } else if(delta < (45*60)) {
+    return (parseInt(delta / 60)).toString() + ' minutes ago';
+  } else if(delta < (90*60)) {
+    return 'an hour ago';
+  } else if(delta < (24*60*60)) {
+    return '' + (parseInt(delta / 3600)).toString() + ' hours ago';
+  } else if(delta < (48*60*60)) {
+    return '1 day ago';
+  } else {
+    return (parseInt(delta / 86400)).toString() + ' days ago';
+  }
 };
 
 
 //get all span.time and recalc from title attribute
 function recalcTime() {
-	$('span.time').each( 
-			function() {
-				$(this).text(relative_time($(this).attr("title")));
-			}
-	)
+  $('span.time').each( 
+      function() {
+        $(this).text(relative_time($(this).attr("title")));
+      }
+  )
 }
 
 
 function getSinceParameter() {
-	if(LAST_UPDATE == null) {
-		return "";
-	} else {
-		return "&since=" + LAST_UPDATE;
-	}
+  if(LAST_UPDATE == null) {
+    return "";
+  } else {
+    return "&since=" + LAST_UPDATE;
+  }
 }
 
 function showAlert(message) {
-	$("#alert p").text(message);
-	$("#alert").fadeIn("fast");
-	return;
+  $("#alert p").text(message);
+  $("#alert").fadeIn("fast");
+  return;
 }
 
 
 function refreshMessages() {
-	showAlert("Getting new tweets...");
-	$(".tweets").gettweets();
-	LAST_UPDATE = new Date().toGMTString();	
-	$("#alert").fadeOut(2000);
-	return;
+  showAlert("Getting new tweets...");
+  $(".tweets").gettweets();
+  LAST_UPDATE = new Date().toGMTString(); 
+  $("#alert").fadeOut(2000);
+  return;
 }
 
 function replyTo(screen_name, msg_id) {
   MSG_ID = msg_id;
   start = '@' + screen_name + ' ';
-	$("#status").val(start);
-	$("#status").focus();
-	$("#status").caret(start.length, start.length);
-	return;
+  $("#status").val(start);
+  $("#status").focus();
+  $("#status").caret(start.length, start.length);
+  return;
 }
 
 function toggleFavorite(id) {
@@ -183,20 +183,20 @@ function setStatus(status_text) {
     MSG_ID = '';
   }
   else {
-  	$.post("http://twitter.com/statuses/update.json", { status: status_text, source: "twoot" }, function(data) { refreshStatusField(); }, "json" );
-	}
-	return;
+    $.post("http://twitter.com/statuses/update.json", { status: status_text, source: "twoot" }, function(data) { refreshStatusField(); }, "json" );
+  }
+  return;
 }
 
 function refreshStatusField() {
-	//maybe show some text below field with last message sent?
-	refreshMessages();
-	$("#status").val("");
-	$('html').animate({scrollTop:0}, 'fast'); 
-	// added by Dr. Drang to reset char count
-	$("#count").removeClass("warning");
+  //maybe show some text below field with last message sent?
+  refreshMessages();
+  $("#status").val("");
+  $('html').animate({scrollTop:0}, 'fast'); 
+  // added by Dr. Drang to reset char count
+  $("#count").removeClass("warning");
   $("#count").addClass("normal");
-	$("#count").html("140");
+  $("#count").html("140");
   return;
 }
 
@@ -224,23 +224,23 @@ function charCountdown() {
 // set up basic stuff for first load
 $(document).ready(function(){
 
-		//get the user's messages
-		refreshMessages();
+    //get the user's messages
+    refreshMessages();
 
-		//add event capture to form submit
-		$("#status_entry").submit(function() {
-			setStatus($("#status").val());
-			return false;
-		});
+    //add event capture to form submit
+    $("#status_entry").submit(function() {
+      setStatus($("#status").val());
+      return false;
+    });
 
-		//set timer to reload messages every 3 minutes
-		window.setInterval("refreshMessages()", 3*60*1000);
+    //set timer to reload messages every 3 minutes
+    window.setInterval("refreshMessages()", 3*60*1000);
 
-		//set timer to recalc timestamps every 60 secs
-		window.setInterval("recalcTime()", 60000);
+    //set timer to recalc timestamps every 60 secs
+    window.setInterval("recalcTime()", 60000);
 
-		//Bind r key to request new messages
-		$(document).bind('keydown', {combi:'r', disableInInput: true}, refreshMessages);
+    //Bind r key to request new messages
+    $(document).bind('keydown', {combi:'r', disableInInput: true}, refreshMessages);
 
 });
 
@@ -256,4 +256,3 @@ function setBottomMargin() {
 
 $(window).load(setBottomMargin);
 $(window).scroll(setBottomMargin);
-		
