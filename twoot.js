f7b2d0bba298edc34737004d95978e0bd6e7745f
Dr. Drang <drdrang@gmail.com>
2014-09-20 22:24:03 -0500

    Remove debugging line.

--- twoot.js
+++ twoot.js
@@ -65,7 +65,6 @@ function htmlify(body, entities, extentities) {
       else if (/^https?:\/\/youtu.be\//.test(u.expanded_url)) {
         idx = u.expanded_url.lastIndexOf(".be/") + 4
         youCode = u.expanded_url.slice(idx);
-        alert(youCode);
         link = '<br /><iframe class="inline" width="320" height="240" src="https://www.youtube.com/embed/' + youCode + '?rel=0" frameborder="0" allowfullscreen></iframe><br />';
         // link = '<a href="' + u.expanded_url + '">' + u.display_url + '</a>';
       }
